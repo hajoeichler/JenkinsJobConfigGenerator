9c1f6559be5b2f0e4cdfc18ac2043698068d5133
Hajo Eichler <hajo.eichler@numberfour.eu>
2011-10-23 14:24:56 +0200

    added minimum generator for boot strapping

---
+++ plugins/de.hajoeichler.jenkins.jobconfig/xtend-gen/de/hajoeichler/jenkins/generator/JobConfigGenerator.java
@@ -0,0 +1,12 @@
+package de.hajoeichler.jenkins.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class JobConfigGenerator implements IGenerator {
+  
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+  }
+}
