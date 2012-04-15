c09db48fc9c1d6be8a52bbc239cd5bb41a471e00
Matthias Köster <matthias.koester@numberfour.eu>
2012-04-15 11:57:05 +0200

    Revert "Revert "further build improvements""
    This reverts commit 0c2ffa0261b4093935d902e2c6ca2dbd74b1ed6f.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mvn --batch-mode -f build/de.hajoeichler.jenkins.jobconfig.platform/pom.xml clean install
-mvn --batch-mode -f plugins/pom.xml -U clean generate-sources -Pgen-xtext
-mvn --batch-mode clean install
+mvn --batch-mode -U -f build/pom.xml clean install
+mvn --batch-mode -U clean generate-sources -Pgen-xtext
+mvn --batch-mode -U install
