993b411fa80395e4e957d4059d428f2deabd5187
Hajo Eichler <hajo.eichler@commercetools.de>
2012-04-15 15:02:57 +0200

    Zip the application.

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+set -e
+
 mvn --batch-mode -U -f build/pom.xml clean install
 mvn --batch-mode -U clean generate-sources -Pgen-xtext
 mvn --batch-mode -U install
+
 cp -R products/de.hajoeichler.jenkins.jobconfig.product/target/products/jenkinsJobConfigGen.product/macosx/cocoa/x86_64/ .
+
+zip -r JenkinsJobConfigGenerator.zip jenkinsJobConfigGenerator/*
