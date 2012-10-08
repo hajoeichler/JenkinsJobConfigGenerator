26e3809cac3cf09668c23306ae1988a033b278eb
Hajo Eichler <hajo.eichler@commercetools.de>
2012-10-08 14:53:15 +0200

    Add test for violations.

--- plugins/de.hajoeichler.jenkins.jobconfig.tests/src/de/hajoeichler/jenkins/generator/MultilineStringFormatingTest.java
+++ plugins/de.hajoeichler.jenkins.jobconfig.tests/src/de/hajoeichler/jenkins/generator/MultilineStringFormatingTest.java
@@ -4,28 +4,33 @@ import org.junit.Test;
 
 public class MultilineStringFormatingTest extends AbstractGeneratorTest {
 
-	@Test
-	public void testEmptyJobConfig() throws Exception {
-		assertSingleConfig("emptyJob", "foo");
-	}
+    @Test
+    public void testEmptyJobConfig() throws Exception {
+        assertSingleConfig("emptyJob", "foo");
+    }
 
-	@Test
-	public void testMultiLineShell() throws Exception {
-		assertSingleConfig("multiLineShell", "multiLineShell");
-	}
+    @Test
+    public void testMultiLineShell() throws Exception {
+        assertSingleConfig("multiLineShell", "multiLineShell");
+    }
 
-	@Test
-	public void overridingOfWrapper() throws Exception {
-		assertSingleConfig("overriddenTimeout", "test-overriddenTimeout");
-	}
+    @Test
+    public void overridingOfWrapper() throws Exception {
+        assertSingleConfig("overriddenTimeout", "test-overriddenTimeout");
+    }
 
-	@Test
-	public void hipChat() throws Exception {
-		assertSingleConfig("hipchat", "JobWithHipChat");
-	}
+    @Test
+    public void hipChat() throws Exception {
+        assertSingleConfig("hipchat", "JobWithHipChat");
+    }
 
-	@Test
-	public void jobWithDisplayName() throws Exception {
-		assertSingleConfig("displayName", "JobWithDisplayName");
-	}
+    @Test
+    public void jobWithDisplayName() throws Exception {
+        assertSingleConfig("displayName", "JobWithDisplayName");
+    }
+
+    @Test
+    public void jobWithCheckstyleViolations() throws Exception {
+        assertSingleConfig("violations", "JobWithViolations");
+    }
 }
